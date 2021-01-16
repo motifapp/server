043d8ddaa5a4f4f215c48efd943c1ecdaac5ef6c
Aiden Bai <mail.contraband@gmail.com>
2021-01-15 19:52:30 -0800

    add caching

--- README.md
+++ README.md
@@ -1,36 +1,3 @@
-# boilerplate
-simple, no-bullshit fullstack web boilerplate
+coldplate
 
-### technologies
-- [eta](https://eta.js.org)
-- [lucia](https://lucia.js.org)
-- [express](https://expressjs.com)
-- [typescript](https://www.typescriptlang.org/)
-- [node](https://nodejs.org)
-
-### install
-
-```
-git clone https://github.com/aidenybai/boilerplate.git
-cd boilerplate
-yarn
-```
-
-or just use vscode 🤡
-
-### instructions
-
-dev start
-```
-yarn dev
-```
-
-prod start
-```
-yarn build && yarn start
-```
-
-prettier
-```
-yarn cleanup
-```
+lunch is ready
--- src/routes/api.ts
+++ src/routes/api.ts
@@ -1,5 +1,11 @@
 import { Router } from 'express';
-import { extract } from 'article-parser';
+
+import fetch from 'node-fetch';
+import stopword from 'stopword';
+import puppeteer from 'puppeteer';
+import normalize from 'normalize-url';
+
+const cache = new Map();
 
 export default {
   path: '/api/v1',
@@ -7,10 +13,45 @@ export default {
     const router = new Router();
 
     router.get('/scrape', async (req, res) => {
-      const url = req.query.url;
-      const page = await extract(url);
+      try {
+        const url: string = req.body.url ?? req.query.url;
+        const response = await fetch(normalize(url));
 
-      res.json({ statusCode: 200, ...page });
+        if (!response.ok) return res.boom.badRequest('Not able to find page');
+
+        if (cache.has(url)) {
+          const content = cache.get(url);
+          return res.json({
+            statusCode: 200,
+            length: content.length,
+            cache: true,
+            content,
+          });
+        }
+
+        const browser = await puppeteer.launch();
+        const page = await browser.newPage();
+        await page.goto(normalize(url), { waitUntil: 'networkidle2' });
+        const raw = await page.evaluate((el) => el.innerText, await page.$('body'));
+        const content = stopword
+          .removeStopwords(raw.split(' '))
+          .join(' ')
+          .replace(/[^a-zA-Z]/g, '')
+          .toLowerCase();
+
+        browser.close();
+
+        cache.set(url, content);
+
+        res.json({
+          statusCode: 200,
+          length: content.length,
+          cache: false,
+          content,
+        });
+      } catch (err) {
+        res.boom.badRequest(err);
+      }
     });
 
     router.get('*', (_req, res) => {
